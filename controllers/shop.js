91e5a587ab7be527bc20e403479f6042242b7059
TMB <Troy.Boettger@gmail.com>
2021-01-15 13:54:28 -0500

    show new details view and configure add to cart option

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 /** @format */
+const { clearCache } = require('ejs')
 const Product = require('../models/product')
 
 exports.getProducts = (req, res, next) => {
@@ -13,8 +14,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
 	const prodId = req.params.productId
-	console.log(prodId)
-	res.redirect('/')
+	Product.findById(prodId, (product) => {
+		res.render('shop/product-detail', {
+			product: product,
+			pageTitle: product.title,
+			path: '/products',
+		})
+	})
 }
 
 exports.getIndex = (req, res, next) => {
@@ -32,6 +38,12 @@ exports.getCart = (req, res, next) => {
 		path: '/cart',
 		pageTitle: 'Your Cart',
 	})
+}
+
+exports.postCart = (req, res, next) => {
+	const prodId = req.body.productId
+	console.log(prodId)
+	res.redirect('/cart')
 }
 
 exports.getOrders = (req, res, next) => {
